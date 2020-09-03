0466516a6e8f45a60900c77557e3629f718f355a
Jorge Cordova <jlcordovat@gmail.com>
2020-09-03 01:51:57 -0500

    Docs Added

---
+++ docs/precache-manifest.8a99fe208823e5903c19d7344e73982d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f4c27c8a38fe143b8487cca34e6b9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "102c5162ab12445f7c8a",
+    "url": "/static/css/main.a028610c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102c5162ab12445f7c8a",
+    "url": "/static/js/main.ba0047dd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
